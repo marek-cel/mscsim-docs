a7b10793da3018a26bfdf90872365ec9b88a9c62
marek-cel <46421808+marek-cel@users.noreply.github.com>
2020-01-12 12:47:36 +0100

    documentation

--- latex/images/gnuplot/run_gnuplot.sh
+++ latex/images/gnuplot/run_gnuplot.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
 ################################################################################
-
-# 123 dpi
-
-################################################################################
 # data
 ################################################################################
+
+gnuplot \
+  openfoam_f16_cx.gnuplot \
+  openfoam_f16_cz.gnuplot
 
 gnuplot \
   vspaero_p51_cm.gnuplot \
   vspaero_p51_cx.gnuplot \
   vspaero_p51_cz.gnuplot
-
-gnuplot \
-  openfoam_f16_cx.gnuplot \
-  openfoam_f16_cz.gnuplot
 
 gnuplot \
   xfoil_naca0012_cm.gnuplot \
